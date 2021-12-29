db2c8119e561887c8a0d41a77f6e4e6e95dc83fb
Andri Tanuarto <andritanuarto@gmail.com>
2021-12-29 01:51:06 -0800

    Remove cx import

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Logo from '../shared/logo/logo';
 import MenuIcon from '@material-ui/icons/Menu';
-import { cx } from '@emotion/css';
 import styles from './main-header.module.scss';
 
 const MainHeader = (): JSX.Element => {
