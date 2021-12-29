cfb630a26632eb7e7ef6e4e72d2eaeabd1c55219
Andri Tanuarto <andritanuarto@gmail.com>
2021-12-29 00:39:48 -0800

    Header styling

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -5,19 +5,10 @@ import { cx } from '@emotion/css';
 import styles from './main-header.module.scss';
 
 const MainHeader = (): JSX.Element => {
-  const [ isNavigationOpen, setIsNavigationOpen ] = useState(false);
-  const logo = (
-    <div className={cx(styles["logo"])}>
-      andri&apos;space
-      <span>UI / UX / DEV</span>
-    </div>
-  )
-
   return (
     <div className={styles.container}>
       <Logo />
       <MenuIcon className={styles.menu} />
-      <button onClick={() => setIsNavigationOpen(!isNavigationOpen)}>adasda</button>
     </div>
   )
 };
